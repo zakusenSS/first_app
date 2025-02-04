bbb1eac14dd65ca2ebe56885af4f6fe3fc4f49bf
zakusenSS <cycpp866@yahoo.co.jp>
2025-02-04 22:12:43 +0900

    posts#indexへのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
-  post 'posts', to: 'posts#create'
- end
- 
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'posts#index'
+end
